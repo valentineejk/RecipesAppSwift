8d429a9a8bccb83c322c9dd0603dea0c965b57fa
miskay <miskayapp@gmail.com>
2022-02-26 16:22:40 +0100

    Initial Commit

---
+++ RecipesAppSwift/RecipesAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppSwiftApp.swift
+//  RecipesAppSwift
+//
+//  Created by Valentineejk on 26/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
